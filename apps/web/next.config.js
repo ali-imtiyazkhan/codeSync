7e0b8f9029963415c3046f7a6bd90f92e10ab2a7
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-04-09 02:01:45 +0530

    fix: added serverComponentsExternalPackages for prisma and extra binaryTargets

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   transpilePackages: ["@codesync/db", "@codesync/socket-types", "@excalidraw/excalidraw"],
 
   experimental: {
+    serverComponentsExternalPackages: ["@prisma/client"],
     turbo: {
       resolveAlias: {
         "@swc/helpers": "@swc/helpers",
