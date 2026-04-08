5cdf356b7cbb6c5f2bd38ec16c9712ef7b89bc04
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-04-09 02:18:29 +0530

    feat: migrate to prisma accelerate and remove query engine

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,17 +1,22 @@
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
+const prismaClientFactory = () => {
+  return new PrismaClient({
     log:
       process.env.NODE_ENV === "development"
         ? ["query", "error", "warn"]
         : ["error"],
-  });
+  }).$extends(withAccelerate());
+};
+
+type PrismaClientWithAccelerate = ReturnType<typeof prismaClientFactory>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientWithAccelerate | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? prismaClientFactory();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
